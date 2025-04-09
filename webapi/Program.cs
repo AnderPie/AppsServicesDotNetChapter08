0d7eaa50678fa9819a6814b6251d8c1f74fb8e68
Ander Pierce <Anderpierce@protonmail.com>
2025-04-09 15:38:06 -0700

    Made a simple Web API

---
+++ webapi/Program.cs
@@ -0,0 +1,25 @@
+using Northwind.EntityModels;
+using Packt.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddNorthwindcontext();
+var app = builder.Build();
+app
+    .MapGets()
+    .MapPosts()
+    .MapPuts()
+    .MapDeletes();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.Run();
